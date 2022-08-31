bbaecfa33ebdb3d4facb153b038e832ec838d705
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2022-08-31 17:18:09 +0200

    Better script variables

--- scripts/annotations_groundtruth.sh
+++ scripts/annotations_groundtruth.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+INDIR=data/annotations
 OUTDIR=/tmp
 # This OUTDIR will rewrite the dataset files:
-#OUTDIR=data/annotations
+# OUTDIR=data/annotations
 
 for TYPE in arousal valence
 do
-    cat data/annotations/annotations.$TYPE.all | python3 scripts/annotations_agreement.py data/triplets.jsonl > $OUTDIR/annotations.$TYPE.all.agreement
+    cat $INDIR/annotations.$TYPE.all | python3 scripts/annotations_agreement.py data/triplets.jsonl > $OUTDIR/annotations.$TYPE.all.agreement
     cat $OUTDIR/annotations.$TYPE.all.agreement | grep -v disagreement | python3 scripts/annotations_triplet_consistency.py > $OUTDIR/annotations.$TYPE.all.agreement.gt
     cat $OUTDIR/annotations.$TYPE.all.agreement.gt | grep \"consistent\" > $OUTDIR/annotations.$TYPE.all.agreement.gt.ct
     cat $OUTDIR/annotations.$TYPE.all.agreement.gt | grep full-agreement > $OUTDIR/annotations.$TYPE.all.agreement.gt.fa
